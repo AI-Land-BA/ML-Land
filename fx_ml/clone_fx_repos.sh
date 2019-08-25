6523f03acfdfba22be5133e98175e023ddce4985
Brian <briancabbott@gmail.coml>
2019-08-25 05:03:03 -0600

    added clone script for primary fx repos.

---
+++ fx_ml/clone_fx_repos.sh
@@ -0,0 +1,41 @@
+mkdir Apache-Singa
+cd Apache-Singa
+git clone https://github.com/apache/incubator-singa
+cd ..
+
+mkdir CNTK
+cd CNTK
+git clone https://github.com/Microsoft/cntk
+cd ..
+
+mkdir Apache-MXNet
+cd Apache-MXNet
+git clone https://github.com/apache/incubator-mxnet
+cd .. 
+
+mkdir ONNX
+cd ONNX
+git clone https://github.com/onnx/models
+git clone https://github.com/onnx/onnx
+git clone https://github.com/onnx/onnx-tensorflow
+git clone https://github.com/onnx/onnx-tensorrt
+git clone https://github.com/onnx/sklearn-onnx
+git clone https://github.com/onnx/tutorials
+cd ..
+
+mkdir Python-Libs
+cd Python-Libs
+git clone https://github.com/Theano/Theano.git
+git clone https://github.com/fastai/fastai
+git clone https://github.com/keras-team/keras/
+git clone https://github.com/matplotlib/matplotlib
+git clone https://github.com/fredrik-johansson/mpmath
+git clone https://github.com/numpy/numpy
+git clone https://github.com/pandas-dev/pandas
+git clone https://github.com/pytorch/pytorch
+git clone git://github.com/scikit-learn/scikit-learn.git
+git clone https://github.com/scipy/scipy
+git clone https://github.com/mwaskom/seaborn
+git clone https://github.com/statsmodels/statsmodels/
+git clone https://github.com/sympy/sympy
+git clone https://github.com/tflearn/tflearn
