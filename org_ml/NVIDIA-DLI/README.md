956200fc9bc09ba2bb320b465b191941adb65124
Brian <briancabbott@gmail.coml>
2019-10-06 21:22:55 -0600

    adding nvidia

---
+++ org_ml/NVIDIA-DLI/README.md
@@ -0,0 +1,4 @@
+https://www.nvidia.com/en-us/deep-learning-ai/education/
+
+https://developer.nvidia.com/open-source
+https://developer.nvidia.com/tensorrt#tensorrt_resources
---
+++ org_ml/NVIDIA-DLI/clone_repos.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/NVIDIA/apex
+
+git clone https://github.com/NVIDIA/DALI
+
+git clone https://github.com/NVIDIA/DeepLearningExamples
+
+git clone https://github.com/NVIDIA/tensorrt-inference-server
