fef624e687d6881beb292dc9b7ebb06b275668b2
Brian <briancabbott@gmail.coml>
2019-10-06 21:14:56 -0600

    lfai components

---
+++ misc-unified/clone_misc_repos.sh
@@ -0,0 +1,10 @@
+
+mkdir Pachi
+cd Pachi
+git clone https://github.com/pasky/pachi
+cd ..
+
+mkdir Pachi-Py
+cd Pachi-Py
+git clone https://github.com/openai/pachi-py
+cd ..
---
+++ org_ml/LFAI/clone_repos.sh
@@ -0,0 +1,29 @@
+
+mkdir Angel-ML
+cd Angel-ML
+git clone https://github.com/Angel-ML/angel.git
+cd ..
+
+
+mkdir EDL
+cd EDL
+git clone https://github.com/PaddlePaddle/edl
+cd ..
+
+
+mkdir Horovod
+cd Horovod
+git clone https://github.com/horovod/horovod
+cd ..
+
+
+mkdir LFAI-Presentations
+cd LFAI-Presentations
+git clone https://github.com/lfai/presentations/
+cd ..
+
+
+mkdir Pyro
+cd Pyro
+git clone https://github.com/pyro-ppl/pyro
+cd ..
